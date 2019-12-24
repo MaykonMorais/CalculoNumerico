ebefb3393fe29e61ee7a6b12f891af0ad82ea52a
unknown <dylan.oli@hotmail.com>
2019-12-24 18:47:19 -0300

    finalizando cholesky

--- Unidade II/arrayHandle.cpp
+++ Unidade II/arrayHandle.cpp
@@ -13,17 +13,21 @@ void vetorGenerate(int lines, float* vet);
 void showMatriz(int lines, int columns, float** matriz);
 void matrizGenerate(int lines, int columns, float ** matriz);
 
-float ** upperTriangular(int lines, int columns, float ** matriz,bool * statusErro);
-float ** lowerTriangular(int lines, int columns, float ** matriz,bool * statusErro);
-
 void operation1(int linesChange, int columns, float ** matriz, float numMult);
 void operation2(int linesChange1,int linesChange2, int columns, float ** matriz);
 void operation3(int linesChange1,int linesChange2, int columns, float ** matriz, float numMult);
 
 void arrayCopy(int lines, int columns, float ** src,float ** target);
+float ** transposed(int size, float ** matriz);
+float ** arrayMutiply(int linesA, int columnsA, int linesB, int columnsB, float ** matrizA, float ** matrizB, bool * statusErro);
+float ** upperTriangular(int lines, int columns, float ** matriz,bool * statusErro);
+float ** lowerTriangular(int lines, int columns, float ** matriz,bool * statusErro);
+
 float determinant(int size, float ** matriz, bool * statusErro);
 bool isSymmetric(int size, float ** matriz);
 bool isPositiveDefinite(int size, float ** matriz);
+
+
 void showVetor(int lines, int* vet)
 {
     int i;
@@ -103,7 +107,7 @@ void showMatriz(int lines, int columns, float** matriz)
         for (int j = 0; j < columns; j++)
         {
             cout.setf(std::ios::fixed,std::ios::floatfield);
-            cout.precision(3);
+            cout.precision(6);
             float num = matriz[i][j];
             if (num<0.0001 && num>-0.0001)
             {
@@ -115,7 +119,7 @@ void showMatriz(int lines, int columns, float** matriz)
         printf("\n");       
     }    
 }
-
+//gera uma matriz com valores aleatórios
 void matrizGenerate(int lines, int columns, float ** matriz)
 {
     srand(time(NULL));
@@ -129,98 +133,7 @@ void matrizGenerate(int lines, int columns, float ** matriz)
         
     }   
 }
-float ** upperTriangular(int lines, int columns, float ** matriz,bool * statusErro)
-{
-    float ** matrizUpper = new float*[lines];
-    for (int i = 0; i < lines; i++)
-    {
-        matrizUpper[i] = new float[columns];
-    }
-    arrayCopy(lines,columns, matriz,matrizUpper);
-    for (int j = 0; j < lines && !*statusErro; j++)
-    {
-        if (matrizUpper[j][j] == 0)// se o elemento da pivotacao for 0 ele tenta trocar por outra linha viavel
-        {
-            bool find = false;
-            for (int i = j+1; i < lines && !find; i++)
-            {
-                if (matrizUpper[i][j]!=0)
-                {
-                    operation2(i,j,columns,matrizUpper);
-                    find = true;
-                }
-            }
-            if (!find)
-            {
-                *statusErro = true; //se ele nao encontrar, nao e possivel triangularizar a matriz
-            }  
-        }
-        if (!*statusErro)
-        {
-            for (int i = j+1; i < lines; i++)
-            {
-                if (matrizUpper[i][j]!=0)
-                {
-                    operation3(i,j,columns,matrizUpper,-matrizUpper[i][j]/matrizUpper[j][j]); //o elementos abaixo do elemento da pivotacao deve ser 0
-                }
-            }
-        }
-    }
-    if (!*statusErro)
-    {
-        return matrizUpper;
-    }
-    else
-    {
-        return NULL;
-    }
-}
-float ** lowerTriangular(int lines, int columns, float ** matriz, bool * statusErro)
-{
-    float ** matrizLower = new float*[lines];
-    for (int i = 0; i < lines; i++)
-    {
-        matrizLower[i] = new float[columns];
-    }
-    arrayCopy(lines,columns, matriz,matrizLower);
-    for (int j = lines-1; j >=0 && !* statusErro; j--)
-    {
-        if (matrizLower[j][j] == 0)// se o elemento da pivotacao for 0 ele tenta trocar por outra linha viavel
-        {
-            bool find = false;
-            for (int i = j-1; i >= 0 && !find; i--)
-            {
-                if (matrizLower[i][j]!=0)
-                {
-                    operation2(i,j,columns,matrizLower);
-                    find = true;
-                }
-            }
-            if (!find)
-            {
-                * statusErro = true; //se ele nao encontrar, nao e possivel triangularizar a matriz
-            }            
-        }
-        if (!* statusErro)
-        {
-            for (int i = j-1; i >=0; i-=1)
-            {
-                if (matrizLower[i][j]!=0)
-                {               
-                    operation3(i,j,columns,matrizLower,-matrizLower[i][j]/matrizLower[j][j]); //o elementos acima do elemento da pivotacao deve ser 0
-                }
-            }
-        }
-    }
-    if (!*statusErro)
-    {
-        return matrizLower;
-    }
-    else
-    {
-        return NULL;
-    }
-}
+
 //---------------------------------Opercoes com Matrizes------------------------------------------------------
 //mutiplica uma linha
 void operation1(int linesChange, int columns, float ** matriz, float numMult)
@@ -275,7 +188,144 @@ void arrayCopy(int lines, int columns, float ** src,float ** target)
     }
     
 }
+float ** arrayMutiply(int linesA, int columnsA, int linesB, int columnsB, float ** matrizA, float ** matrizB, bool * statusErro)
+{
+    float ** matrizMutiply = new float*[linesA];
+    for (int i = 0; i < linesA; i++)
+    {
+        matrizMutiply[i] = new float[columnsB];
+    }
+    if (columnsA == linesB)
+    {
+        for (int i = 0; i < linesA; i++)
+        {
+            for (int j = 0; j < columnsB; j++)
+            {
+                float result = 0;
+                for (int k = 0; k < columnsA; k++)
+                {
+                    result += matrizA[i][k]*matrizB[k][j];
+                }
+                matrizMutiply[i][j] = result;
+            }
+        }
+    }
+    else
+    {
+        *statusErro = true;
+    }
+    return matrizMutiply;
+}
+float ** upperTriangular(int lines, int columns, float ** matriz,bool * statusErro)
+{
+    float ** matrizUpper = new float*[lines];
+    for (int i = 0; i < lines; i++)
+    {
+        matrizUpper[i] = new float[columns];
+    }
+    arrayCopy(lines,columns, matriz,matrizUpper);
+    for (int j = 0; j < lines && !*statusErro; j++)
+    {
+        if (matrizUpper[j][j] == 0)// se o elemento da pivotacao for 0 ele tenta trocar por outra linha viavel
+        {
+            bool find = false;
+            for (int i = j+1; i < lines && !find; i++)
+            {
+                if (matrizUpper[i][j]!=0)
+                {
+                    operation2(i,j,columns,matrizUpper);
+                    find = true;
+                }
+            }
+            if (!find)
+            {
+                *statusErro = true; //se ele nao encontrar, nao e possivel triangularizar a matriz
+            }  
+        }
+        if (!*statusErro)
+        {
+            for (int i = j+1; i < lines; i++)
+            {
+                if (matrizUpper[i][j]!=0)
+                {
+                    operation3(i,j,columns,matrizUpper,-matrizUpper[i][j]/matrizUpper[j][j]); //o elementos abaixo do elemento da pivotacao deve ser 0
+                }
+            }
+        }
+    }
+    if (!*statusErro)
+    {
+        return matrizUpper;
+    }
+    else
+    {
+        return NULL;
+    }
+}
 
+float ** lowerTriangular(int lines, int columns, float ** matriz, bool * statusErro)
+{
+    float ** matrizLower = new float*[lines];
+    for (int i = 0; i < lines; i++)
+    {
+        matrizLower[i] = new float[columns];
+    }
+    arrayCopy(lines,columns, matriz,matrizLower);
+    for (int j = lines-1; j >=0 && !* statusErro; j--)
+    {
+        if (matrizLower[j][j] == 0)// se o elemento da pivotacao for 0 ele tenta trocar por outra linha viavel
+        {
+            bool find = false;
+            for (int i = j-1; i >= 0 && !find; i--)
+            {
+                if (matrizLower[i][j]!=0)
+                {
+                    operation2(i,j,columns,matrizLower);
+                    find = true;
+                }
+            }
+            if (!find)
+            {
+                * statusErro = true; //se ele nao encontrar, nao e possivel triangularizar a matriz
+            }            
+        }
+        if (!* statusErro)
+        {
+            for (int i = j-1; i >=0; i-=1)
+            {
+                if (matrizLower[i][j]!=0)
+                {               
+                    operation3(i,j,columns,matrizLower,-matrizLower[i][j]/matrizLower[j][j]); //o elementos acima do elemento da pivotacao deve ser 0
+                }
+            }
+        }
+    }
+    if (!*statusErro)
+    {
+        return matrizLower;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+//transposição de matriz
+float ** transposed(int size, float ** matriz)
+{
+    float ** matrizTransposed = new float*[size];
+    for (int i = 0; i < size; i++)
+    {
+        matrizTransposed[i] = new float[size];
+    }
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            matrizTransposed[i][j] = matriz[j][i];
+        }
+    }
+    return matrizTransposed;
+}
 //---------------------------------------FIM------------------------------------------------------------------
 float determinant(int size, float ** matriz, bool * statusErro)
 {
--- Unidade II/cholesky.cpp
+++ Unidade II/cholesky.cpp
@@ -11,16 +11,23 @@ int main()
     cout<<"Digite a ordem da matriz: ";
     cin>>SIZE;
     bool statusErro = false;
-    float ** matriz, ** matrizL;
-    matriz = new float*[SIZE];
-    for (int i = 0; i < SIZE; i++)
-    {
-        matriz[i] = new float[SIZE+1];
-    }
+    float ** matriz, ** matrizL, ** matrizLT;
+    float *vetY, *vetX, *vetResult;
+    
+    vetY = new float[SIZE];
+    vetX = new float[SIZE];
+    vetResult = new float[SIZE];
+
     matrizL = new float*[SIZE];
     for (int i = 0; i < SIZE; i++)
     {
-        matrizL[i] = new float[SIZE+1];
+        matrizL[i] = new float[SIZE];
+    }
+
+    matriz = new float*[SIZE];
+    for (int i = 0; i < SIZE; i++)
+    {
+        matriz[i] = new float[SIZE];
     }
     char Choice;
 
@@ -29,45 +36,87 @@ int main()
     cin>>Choice;
     if (Choice == 's' || Choice == 'S')
     {
-        matrizGenerate(SIZE,SIZE+1,matriz);
+        matrizGenerate(SIZE,SIZE,matriz);
+        vetorGenerate(SIZE,vetResult);
     }
     else
     {
         for (int i = 0; i < SIZE; i++)
         {
-            for (int j = 0; j < SIZE+1; j++)
+            for (int j = 0; j < SIZE; j++)
             {
                 cout<<"Posicao["<<i<<"]["<<j<<"]:";
                 cin>>matriz[i][j];
             }
         }
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            cout<<"Resultado["<<i<<"]:";
+            cin>>vetResult[i];
+        }
     }
 
-    cout<<"Matriz Original:\n";
-    showMatriz(SIZE,SIZE+1,matriz);
+    cout<<"\nMatriz Original:\n";
+    showMatriz(SIZE,SIZE,matriz);
 
-    //triangularizacao
     statusErro = !isPositiveDefinite(SIZE,matriz);
     if (!statusErro)
     {
-        cout<<"A matriz L:\n";
+        // matrizL = lowerTriangular(SIZE,SIZE,matriz,&statusErro);
         matrizL[0][0] = sqrt(matriz[0][0]);
         showMatriz(SIZE,SIZE,matrizL);
-        // showMatriz(SIZE,SIZE+1,matrizUpper); //exibe a matriz final
-        // float * x = new float[SIZE];
-        // for (int i = SIZE-1; i >= 0; i--)
-        // {
-        //     x[i] = matrizUpper[i][SIZE];
-        //     for (int j = i+1; j < SIZE; j++)
-        //     {
-        //         x[i] -= matrizUpper[i][j]*x[j];
-        //     }
-        //     x[i] /= matrizUpper[i][i];        
-        // }
-        // for (size_t i = 0; i < SIZE; i++)
-        // {
-        //     cout<<"x"<<i+1<<" = "<<x[i]<<endl; 
-        // } 
+        cout<<endl;
+        for (int i = 1; i < SIZE; i++)
+        {
+            matrizL[i][0] = matriz[i][0]/matrizL[0][0];
+            showMatriz(SIZE,SIZE,matrizL);
+            cout<<endl;
+        }
+        for (int i = 1; i < SIZE; i++)
+        {
+            float sum = 0;
+            for (int j = 0; j < i; j++)
+            {
+                float sum2 = 0;
+                for (int k = 0; k < j; k++)
+                {
+                    sum2 += matrizL[i][k]*matrizL[j][k];
+                }
+                matrizL[i][j] = (matriz[i][j] - sum2)/matrizL[j][j];
+                sum += pow(matrizL[i][j],2);
+            }
+            matrizL[i][i] = sqrt(matriz[i][i]-sum);
+        }
+        matrizLT = transposed(SIZE, matrizL);
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            vetY[i] = vetResult[i];
+            for (int j = i-1; j >= 0; j--)
+            {
+                vetY[i] -= matrizL[i][j]*vetY[j];
+            }
+            vetY[i] /= matrizL[i][i];        
+        }
+
+        for (int i = SIZE-1; i >= 0; i--)
+        {
+            vetX[i] = vetY[i];
+            for (int j = i+1; j < SIZE; j++)
+            {
+                vetX[i] -= matrizLT[i][j]*vetX[j];
+            }
+            vetX[i] /= matrizLT[i][i];        
+        }
+        cout<<"\nMatriz L:\n";
+        showMatriz(SIZE,SIZE,matrizL);
+        cout<<"\nMatriz Lt:\n";
+        showMatriz(SIZE,SIZE,matrizLT);
+        cout<<"\nVetor Y:\n";
+        showVetor(SIZE,vetY);
+        cout<<"\nVetor X:\n";
+        showVetor(SIZE,vetX);
     }
     else
     {
